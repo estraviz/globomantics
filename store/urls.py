51a89d2babcf4a24d3ab9eebdcd980324f2cedb7
Javier Estraviz <javier.estraviz@gmail.com>
2023-08-24 01:06:35 +0200

    Added class-based views (view, list view, template view)

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path, re_path
 
 from . import views
+from .views import (
+    ElectronicsView,
+    ElectronicsTemplateView,
+    ElectronicsListView
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    re_path(r'^\d+', views.detail, name='detail'),
-    re_path(r'^electronics', views.electronics, name='electronics'),
+    path(route='', view=views.index, name='index'),
+    re_path(route=r'^\d+', view=views.detail, name='detail'),
+    re_path(route=r'^electronics-function-view$',
+            view=views.electronics, name='electronics-function-view'),
+    re_path(route=r'^electronics-class-view$',  view=ElectronicsView.as_view(),
+            name='electronics-class-view'),
+    re_path(route=r'^electronics-template-view$',
+            view=ElectronicsTemplateView.as_view(),
+            name='electronics-template-view'),
+    re_path(route=r'^electronics-list-view$',
+            view=ElectronicsListView.as_view(), name='electronics-list-view'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,17 @@
 from django.core.paginator import PageNotAnInteger, Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
+from django.views import View
 from django.views.decorators.cache import cache_page
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
+from django.views.generic import ListView, TemplateView
 
 
 def index(request):
-    return HttpResponse("Hello there, globomantics e-commerce store front coming here...")
+    return HttpResponse(
+        "Hello there, globomantics e-commerce store front coming here..."
+    )
 
 
 def detail(request):
@@ -18,7 +22,9 @@ def detail(request):
 @cache_page(900)
 @require_http_methods(["GET"])
 def electronics(request):
-    items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
+    items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung",
+             "Google")
+
     if request.method == 'GET':
         paginator = Paginator(items, 2)
         pages = request.GET.get('page', 1)
@@ -29,3 +35,38 @@ def electronics(request):
         return render(request, 'store/list.html', {'items': items})
     elif request.method == 'POST':
         return HttpResponseNotFound("POST method is not allowed, dude")
+
+
+class ElectronicsView(View):
+    def get(self, request):
+        items = ("Android", "Slimbook", "Apple iPad", "Dell", "Motorola",
+                 "Toshiba")
+
+        if request.method == 'GET':
+            paginator = Paginator(items, 2)
+            pages = request.GET.get('page', 1)
+            try:
+                items = paginator.page(pages)
+            except PageNotAnInteger:
+                items = paginator.page(1)
+            return render(request, 'store/list.html', {'items': items})
+        elif request.method == 'POST':
+            return HttpResponseNotFound("POST method is not allowed, dude")
+
+
+class ElectronicsTemplateView(TemplateView):
+    template_name = 'store/list.html'
+
+    def get_context_data(self, **kwargs):
+        items = ("Verbatim", "Maxtor", "Kindle", "Sandisk", "Raspberry Pi",
+                 "Remarkable")
+        context = {'items': items}
+        return context
+
+
+class ElectronicsListView(ListView):
+    template_name = 'store/list2.html'
+
+    queryset = ("LG", "Garmin", "Suntoo", "Belkin", "Arduino", "Apple Pencil")
+    context_object_name = 'items'
+    paginate_by = 2
